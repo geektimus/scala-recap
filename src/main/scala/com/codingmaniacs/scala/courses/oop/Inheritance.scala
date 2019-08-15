26270d4c52c0e05003fcc8622677a251492efe62
Geektimus <alexander.kno+github@gmail.com>
2019-08-14 19:43:57 -0500

    Add inheritance exercise (Custom List)

--- src/main/scala/com/codingmaniacs/scala/courses/oop/Inheritance.scala
+++ src/main/scala/com/codingmaniacs/scala/courses/oop/Inheritance.scala
@@ -1,13 +1,37 @@
+/*
+ * Copyright (c) 2019 Geektimus
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
 package com.codingmaniacs.scala.courses.oop
 
 import scala.annotation.tailrec
 
 object Inheritance {
+
   abstract class MyList[T] {
     def head: T
     def tail: MyList[T]
     def isEmpty: Boolean
-    def add(n: T): MyList[T]
+
+    def prepend(n: T): MyList[T]
+    def reverse: MyList[T]
 
     protected def prettyPrint: String
     override def toString: String = s"[$prettyPrint]"
@@ -20,7 +44,9 @@ object Inheritance {
 
     override def isEmpty: Boolean = true
 
-    override def add(n: T): MyList[T] = new CustomList(n, EmptyList())
+    override def prepend(n: T): MyList[T] = new CustomList(n, EmptyList())
+
+    override def reverse: MyList[T] = EmptyList()
 
     override protected def prettyPrint: String = ""
   }
@@ -36,7 +62,18 @@ object Inheritance {
 
     override def isEmpty: Boolean = false
 
-    override def add(n: T): MyList[T] = new CustomList(n, this)
+    override def prepend(n: T): MyList[T] = new CustomList(n, this)
+
+    override def reverse: MyList[T] = {
+
+      @tailrec
+      def reverseRec(res: MyList[T], rem: MyList[T]): MyList[T] =
+        rem match {
+          case _: EmptyList[T] => res
+          case l => reverseRec(res.prepend(l.head), l.tail)
+        }
+      reverseRec(EmptyList(), this)
+    }
 
     override def prettyPrint: String = {
 
@@ -61,17 +98,9 @@ object Inheritance {
       def concatRec(res: MyList[T], rem: Seq[T]): MyList[T] =
         rem match {
           case Seq() => res
-          case Seq(head, tail @ _*) => concatRec(res.add(head), tail)
+          case Seq(head, tail @ _*) => concatRec(res.prepend(head), tail)
         }
       concatRec(EmptyList(), xs)
     }
-  }
-
-  def main(args: Array[String]): Unit = {
-
-    val myList = CustomList[Int](1,2,3,4,5,6)
-
-    println(myList.toString)
-
   }
 }
---
+++ src/test/scala/com/codingmaniacs/scala/courses/oop/InheritanceSpec.scala
@@ -0,0 +1,74 @@
+/*
+ * Copyright (c) 2019 Geektimus
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package com.codingmaniacs.scala.courses.oop
+
+import com.codingmaniacs.scala.courses.oop.Inheritance.{ CustomList, EmptyList }
+import org.specs2.matcher.AnyMatchers
+import org.specs2.mutable.Specification
+
+class InheritanceSpec extends Specification with AnyMatchers {
+  "The custom list" should {
+    "allow the creation of an EmptyList" in {
+      val emptyList = EmptyList()
+      emptyList must not be null
+      emptyList.isEmpty must beTrue
+    }
+    "allow the user to add elements to an EmptyList" in {
+      val list = EmptyList().prepend(2)
+      list.isEmpty must beFalse
+      list.head mustEqual 2
+      list.tail.isEmpty must beTrue
+    }
+    "allow the user to create a non empty list" in {
+      val list = new CustomList(1, new CustomList(2, EmptyList()))
+      list.isEmpty must beFalse
+      list.head mustEqual 1
+      val tail = list.tail
+      tail.head mustEqual 2
+      tail.tail.isEmpty must beTrue
+    }
+    "allow the user to reverse an empty list (duh!)" in {
+      val emptyList = EmptyList()
+      emptyList.isEmpty must beTrue
+      emptyList.reverse.isEmpty must beTrue
+    }
+    "allow the user to reverse a custom list" in {
+      val list = new CustomList(1, new CustomList(2, EmptyList()))
+      list.isEmpty must beFalse
+      val reversedList = list.reverse
+      reversedList.head mustEqual 2
+      val tail = reversedList.tail
+      tail.head mustEqual 1
+      tail.tail.isEmpty must beTrue
+    }
+    "allow the user to print the elements of a custom list" in {
+      val list = new CustomList(1, new CustomList(2, EmptyList()))
+      list.isEmpty must beFalse
+      list.toString mustEqual "[1,2]"
+    }
+    "allow create a list in a easy way" in {
+      val list = CustomList(1, 2, 3, 4, 5, 6)
+      list.isEmpty must beFalse
+      list.toString mustEqual "[6,5,4,3,2,1]"
+    }
+  }
+}
