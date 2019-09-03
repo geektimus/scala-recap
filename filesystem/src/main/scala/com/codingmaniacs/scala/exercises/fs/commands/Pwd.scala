fd7e7174779b9147447f6e82dea90f1e62530e66
Geektimus <alexander.kno+github@gmail.com>
2019-09-03 14:48:43 -0500

    PWD command implementation

--- filesystem/src/main/scala/com/codingmaniacs/scala/exercises/fs/commands/Pwd.scala
+++ filesystem/src/main/scala/com/codingmaniacs/scala/exercises/fs/commands/Pwd.scala
@@ -20,44 +20,8 @@
  */
 
 package com.codingmaniacs.scala.exercises.fs.commands
-
 import com.codingmaniacs.scala.exercises.fs.State
 
-trait Command {
-  def apply(state: State): State
-}
-
-object Command {
-
-  val MKDIR = "mkdir"
-  val LS = "ls"
-  val PWD = "pwd"
-
-  def emptyCommand: Command = new Command {
-    override def apply(state: State): State = state
-  }
-
-  def incompleteCommand(name: String): Command = new Command {
-    override def apply(state: State): State =
-      state.setMessage(s"$name is an incomplete command")
-  }
-
-  def from(input: String): Command = {
-    val tokens = input.split(" ")
-    if (input.isEmpty || tokens.isEmpty) {
-      emptyCommand
-    } else if (MKDIR.equals(tokens(0))) {
-      if (tokens.length < 2) {
-        incompleteCommand(MKDIR)
-      } else {
-        new MkDir(tokens(1))
-      }
-    } else if (LS.equals(tokens(0))) {
-      new Ls()
-    } else if (PWD.equals(tokens(0))) {
-      new Pwd()
-    } else {
-      new UnknownCommand
-    }
-  }
+class Pwd extends Command {
+  override def apply(state: State): State = state.setMessage(state.workingDir.path)
 }
