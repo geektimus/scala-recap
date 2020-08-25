80c9b1edc5f11d2400f4ab4c7b46622a34925811
Geektimus <alexander.kno+github@gmail.com>
2020-08-24 23:17:02 -0500

    Add exercises to implemente fns toCurry, fromCurry, compose, andThen

--- src/main/scala/com/codingmaniacs/scala/courses/basic/FunctionExercises.scala
+++ src/main/scala/com/codingmaniacs/scala/courses/basic/FunctionExercises.scala
@@ -23,9 +23,12 @@ package com.codingmaniacs.scala.courses.basic
 
 object FunctionExercises {
 
-  def toCurry(f: (Int, Int) => Int): Int => Int => Int = {
-    val g = (a: Int) => f(a, _)
-    g
-  }
+  def fromCurry(f: Int => Int => Int): (Int, Int) => Int = (a, b) => f(a)(b)
+
+  def toCurry(f: (Int, Int) => Int): Int => Int => Int = a => b => f(a, b)
+
+  def compose[A, B, C](f: A => B, g: C => A): C => B = x => f(g(x))
+
+  def andThen[A, B, C](f: A => B, g: B => C): A => C = x => g(f(x))
 
 }
--- src/test/scala/com/codingmaniacs/scala/courses/basic/FunctionExercisesTest.scala
+++ src/test/scala/com/codingmaniacs/scala/courses/basic/FunctionExercisesTest.scala
@@ -33,4 +33,33 @@ class FunctionExercisesTest extends AnyFunSuite with ScalaCheckDrivenPropertyChe
       assert(curried(x)(y) == f(x, y))
     }
   }
+
+  test("The function is always uncurried") {
+    forAll { (x: Int, y: Int) =>
+      val f = (x: Int) => (y: Int) => x + y
+      val uncurried = FunctionExercises.fromCurry(f)
+      assert(uncurried(x, y) == f(x)(y))
+    }
+  }
+
+  test("We can compose two functions using compose") {
+    forAll { (a: Int) =>
+      val f = (x: Int) => x * 10
+      val g = (x: Int) => x / 2
+
+      val composed = FunctionExercises.compose(f, g)
+      assert(composed(a) == (f compose g)(a))
+    }
+  }
+
+  test("We can compose two functions using andThen") {
+    forAll { (a: Int) =>
+      val f = (x: Int) => x + x
+      val g = (x: Int) => x * 3
+
+      val andThen = FunctionExercises.andThen(f, g)
+      assert(andThen(a) == (g andThen f)(a))
+    }
+  }
+
 }
